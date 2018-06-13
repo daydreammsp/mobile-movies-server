31753326b41db15860991141061ae7968b83b16f
Jonathan Kruse <jonathanskruse@gmail.com>
2018-06-12 20:39:58 -0500

    api info on dom

---
+++ server/routes/movie.router.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+const request = require("request");
+
+router.get('/', (req, res) => {
+    console.log('router request')
+    let options = { method: 'GET',
+  url: 'https://api.themoviedb.org/3/search/movie',
+  qs: 
+   { api_key: '82a12a54b5388a78460f43520ffc035e',
+     query: 'action',
+     language: 'en-US',
+     page: '1',
+     include_adult: 'true' },
+  headers: 
+   { 'Cache-Control': 'no-cache',
+     Accept: 'application/json',
+     Authorization: 'Bearer sq0atp--xOe6YM9OchfFi_-1UURRw',
+     'Content-Type': 'application/x-www-form-urlencoded' },
+  form: false };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+let newBody = JSON.parse(body)
+let results = newBody.results.map( (item)=>{
+    return item.title
+})
+  console.log('results', results);
+  res.send(results)
+});
+    })
+
+
+    module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,40 @@
+
+const express = require('express');
+
+
+const app = express();
+const bodyParser = require('body-parser');
+// const sessionMiddleware = require('./modules/session-middleware');
+
+// const passport = require('./strategies/user.strategy');
+
+// Route includes
+ const movieRouter = require('./routes/movie.router');
+//api router
+// const squareRouter = require('./routes/square.router');
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Passport Session Configuration //
+// app.use(sessionMiddleware);
+
+// start up passport sessions
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+/* Routes */
+app.use('/', movieRouter);
+// app.use('/api/square', squareRouter);
+
+// Serve static files
+app.use(express.static('build'));
+
+// App Set //
+const PORT = process.env.PORT || 5002;
+
+/** Listen * */
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
