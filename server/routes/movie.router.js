9dff6461e2fc4f042f9672ba97ac6c37b180db09
Jonathan Kruse <jonathanskruse@gmail.com>
2018-08-08 22:18:08 -0400

    mailer

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 const request = require("request");
+const nodemailer = require('nodemailer');
 
 
     router.post('/api', (req, res) => {
@@ -42,4 +43,40 @@ const request = require("request");
     });
         })
 
+        router.post('/api/form', (req, res) => {
+          console.log(req.body)
+          
+              // create reusable transporter object using the default SMTP transport
+              let transporter = nodemailer.createTransport({
+                  host: 'smtp.gmail.com',
+                  port: 587,
+                  secure: false, // true for 465, false for other ports
+                  auth: {
+                      user: 'jskruseportfoliosite', // generated ethereal user
+                      pass: 'portfoliosite' // generated ethereal password
+                  }
+              });
+          
+              // setup email data with unicode symbols
+              let mailOptions = {
+                  from: 'jskruseportfoliosite@gmail.com', // sender address
+                  to: 'jonathanskruse@gmail.com', // list of receivers
+                  subject: req.body.subject, // Subject line
+                  html: '<h2>'+req.body.name +'</h2>'+'<h2>'+req.body.email +'</h2>'+'<h2>'+req.body.subject +'</h2>'+'<h2>'+req.body.message +'</h2>' // html body
+              };
+          
+              // send mail with defined transport object
+              transporter.sendMail(mailOptions, (error, info) => {
+                  if (error) {
+                      return console.log(error);
+                  }
+                  console.log('Message sent: %s', info.messageId);
+                  // Preview only available when sending through an Ethereal account
+                  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+          res.send('OK')
+                  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+                  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+              });
+          
+      });
     module.exports = router;
